f4e43e831a7a521d6b2d2dec7ce4a5c98f5bf9c3
Tenente Colombo <ferrerocarlo96@gmail.com>
2021-05-30 18:17:06 +0200

    :wq
    :wq

---
+++ src/components/fullscreen-slider/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const fullscreenSlider = () => {
+    return (
+        <div>
+            <h1>This is a fullscreen slider</h1>
+        </div>
+    )
+}
+
+export default fullscreenSlider
